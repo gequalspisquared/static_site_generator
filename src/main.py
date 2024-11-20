ed1b6ab25982d00a3c77285180a83b6c19dba2c4
Nicholas Crane <nickcrusher30@gmail.com>
2024-11-19 20:22:01 -0700

    Create main function and script and create basic textnode class
    Create main.py and main.sh to run everything. TextNode will be
    responsible for representing inline text.

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import *
+
+def main():
+    print("hello world")
+    node = TextNode("This is a text node", TextType.NORMAL, "https://www.boot.dev")
+    print(node)
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = 1
+    BOLD = 2
+    ITALIC = 3
+    CODE = 4
+    LINKS = 5
+    IMAGES = 6
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other) -> bool:
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
